e9b886f3962e8125226f07e3c692c8fedbf4e6a8
gianfrancosg <gianfrancosantinato@hotmail.com>
2022-08-09 01:00:24 -0400

    Added the checkbox to the bottom of the page

--- src/components/algorithmPage/AlgorithmPage.js
+++ src/components/algorithmPage/AlgorithmPage.js
@@ -1,9 +1,11 @@
 import './AlgorithmPage.css';
 import React, { useState } from 'react';
+import { Checkbox, FormControlLabel, Typography } from "@material-ui/core";
 
 // Child Components
 import InformationPage from '../informationPage/InformationPage';
 import Visualizer from '../visualizer/Visualizer';
+
 
 function NavigateToDashboard(){
 	window.location.href = "/";
@@ -12,15 +14,37 @@ function NavigateToDashboard(){
 function AlgorithmPage({ path, type, algoPage }) {
 
 	const[show, setShow] = useState(false);
+	const [val, setVal] = useState(false);
 
 	return (
 		<div className='AlgorithmPage'> 
 			<Visualizer path={path} type={type}/>
 			<br/>
-			<div class="button-location">
-				<button class="button" onClick={NavigateToDashboard}>Dashboard</button>
-				<button class="button2" onClick={()=> setShow(!show)}>More Information</button>
-				{show && <InformationPage path={path} />}
+			<div className='container'>
+				<div className="button-location">
+					<button class="button" onClick={NavigateToDashboard}>Dashboard</button>
+					<button class="button2" onClick={()=> setShow(!show)}>More Information</button>
+					{show && <InformationPage path={path} />}
+				</div>
+				<div className='checkbox-location'>
+					<FormControlLabel
+						control={
+							<Checkbox
+								checked={val}
+								onChange={(e) => setVal(e.target.checked)}
+								style={{
+								color: "#FFC904"
+								}}
+								value="Completed?"
+							/>
+						}
+						label={
+						<Typography variant='h5' style={{ color: "#FFFFFF" }}>
+							<h4>Completed?</h4>
+						</Typography>
+						}
+					/>
+				</div>
 			</div>
 			<br/><br/>
 			

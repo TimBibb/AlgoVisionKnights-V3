79003180d26373a7141681c25b2cf96683b95edb
Johnny Ngo <johnnyhngo@yahoo.com>
2022-08-30 12:26:27 -0400

    SLL D3 visual update

--- src/visualizers/singlylinkedlist/singlylinkedlist.js
+++ src/visualizers/singlylinkedlist/singlylinkedlist.js
@@ -302,20 +302,50 @@ export default class singlylinkedlist extends React.Component {
 		let arr = [0, 0, 0, 0, 0, 0, 0, 0]
 		let arrLine = [0, 0, 0, 0, 0, 0, 0]
 
-		// nodes
-		let nodes = svg.selectAll('rect')
+		let containers = svg.selectAll(".containers")
 			.data(arr)
-			.enter().append('rect')
-			.style("fill", "gray")
+			.enter()
+			.append("g")
+			.attr("class", "gbar")
+		
+		containers.append('rect')
 			.attr('height', barHeight)
 			.attr('width', barWidth)
+			// Each box is ith 0 to 8
 			.attr('x', function (d, i) { return 150 * i; })
 			.attr('y', '50')
-			.attr("stroke", "rgb(255,255,255)")
+			.style("fill", "rgb(153,204,255)")
 			.attr("stroke-width", "2")
+			.attr("stroke", "grey")
+				
+
+		containers.append("text")
+			.text((d) => {
+				console.log("BAR" + d);
+				return d;
+			})
+			.attr("y", '83')
+			.attr("x", function (d,i) { return (150 * i) + 20})
+			.style("text-anchor", "middle")
+			.style("font-size", "28px")
+			.style("fill", "white");
+
 		
-		// arrows
-		svg.append("svg:defs").append("svg:marker")
+		containers.append('line')
+			.data(arrLine)
+			.style("stroke", "white")
+			.style("stroke-width", 5)
+			.attr("x1", function (d, i) {
+				d = 90
+				return (d + 150 * (i))
+			})
+			.attr("y1", 75)
+			.attr("x2", function (d, i) { return (150 * (i + 1)) })
+			.attr("y2", 75)
+			.attr("marker-end", "url(#arrow)")
+
+
+		containers.append("svg:defs").append("svg:marker")
 			.attr("id", "arrow")
 			.attr("viewBox", "0 0 12 12")
 			.attr("refX", 10.5)
@@ -328,20 +358,10 @@ export default class singlylinkedlist extends React.Component {
 			.attr("d", "M2,2 L10,6 L2,10 L6,6 L2,2")
 			.style("stroke", "white");
 
-		// links
-		let links = svg.selectAll('line')
-			.data(arrLine)
-			.enter().append('line')
-			.style("stroke", "white")
-			.style("stroke-width", 5)
-			.attr("x1", function (d, i) {
-				d = 90
-				return (d + 150 * (i))
-			})
-			.attr("y1", 75)
-			.attr("x2", function (d, i) { return (150 * (i + 1)) })
-			.attr("y2", 75)
-			.attr("marker-end", "url(#arrow)")
+		
+
+
+		
 
 		
 		// Triggers componentDidUpdate to run the main function for the steps and messages queues
@@ -465,6 +485,8 @@ export default class singlylinkedlist extends React.Component {
 	}
 
 
+
+
 	render() {
 		return (
 			<div>
@@ -489,3 +511,4 @@ export default class singlylinkedlist extends React.Component {
 		)
 	}
 }
+
